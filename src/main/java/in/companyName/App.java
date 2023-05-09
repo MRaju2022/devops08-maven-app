3ec6ea712285bb333a8a2401cba4399f99fb26ab
Raju <raju@gamil.com>
2023-05-09 08:19:18 +0530

    Task-1001 changes

--- src/main/java/in/companyName/App.java
+++ src/main/java/in/companyName/App.java
@@ -9,6 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		
+		//Changes are related to task - 1001
+		
+		System.out.println("Welcome to Devops with AWS classes")
     }
 	
 	//These changes are related to Task-1002
